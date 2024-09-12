82d0ab5016dd55f68f38a1a554b2ece861df69ea
Youssef <youssefahmed505505@gmail.com>
2024-09-12 12:12:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mealsapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mealsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mealsapp.ui.screens.meals.MealsScreen
+import com.example.mealsapp.ui.theme.MealsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MealsAppTheme {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    MealsScreen()
+                }
+            }
+        }
+    }
+}
